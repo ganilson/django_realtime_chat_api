c111a9295ca3b888f30ff1338295a840a207e418
TecnoBase <ganilsong87@gmail.com>
2024-09-27 16:10:38 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -44,12 +44,23 @@ INSTALLED_APPS = [
 
 ]
 ASGI_APPLICATION = 'config.asgi.application'
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",  # Para produção, pode-se usar Redis ou outro backend
-    },
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels.layers.InMemoryChannelLayer",  # Para produção, pode-se usar Redis ou outro backend
+#     },
     
+# }
+
+#For PRODUCTION!!
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],  # URL do Redis
+        },
+    },
 }
+
 AUTH_USER_MODEL = "app.User"
 
 MIDDLEWARE = [
